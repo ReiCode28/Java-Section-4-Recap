4275e682c1e6ded6afdac6bef461838463b59d28
randy <randyr_1993@yahoo.com>
2022-09-26 00:39:18 -0400

    Completed Weather

--- Weather.java
+++ Weather.java
@@ -4,13 +4,23 @@ public class Weather {
         double evening = 61;       //temperature during the evening in fahrenheit
         double midnight = 55;      //temperature at midnight in fahrenheit
 
-        //Task 3 - Call the printTemperatures function. 
+        printTemperatures(noon);
+        printTemperatures(evening);
+        printTemperatures(midnight);
         
         
     }
 
+    public static double fahrenheitToCelsius(double fahrenheit){
+        double celsius = (fahrenheit - 32) * 5/9;
+        return celsius;
+    }
 
-    //Task 1: Make a function here. See the doc comments below. 
+    public static void printTemperatures(double fahrenheit){
+        System.out.println("F: " + fahrenheit);
+        System.out.println("C: " + fahrenheitToCelsius(fahrenheit) + "\n");
+    }
+
 
     /**
      * Function name: fahrenheitToCelsius - converts fahrenheit to celcius
@@ -21,8 +31,7 @@ public class Weather {
      * 1. returns the temperature in celcius. C = (F - 32) * 5/9. 
      */
 
-     
-    //Task 2: Make a function here. See the doc comments below. 
+      
 
     /**
      * Function name: printTemperatures – prints both temperature values. 
