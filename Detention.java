0ed3782a16e723c240c6ce1c7ab86adbcdc4924e
randy <randyr_1993@yahoo.com>
2022-09-25 22:56:39 -0400

    Completed Detention

--- Detention.java
+++ Detention.java
@@ -1,25 +1,33 @@
+/**
+ * Bart Simpson is in detention and his teacher wants him to write lines on the 
+ * chalkboard. Bart needs to write a paragraph 6 times, help him by writing 
+ * a function that does the task for him.
+ */
+
 public class Detention {
     public static void main(String[] args) {
 
-        //what Bart did so far
-        System.out.println("I will not copy and paste code.");
-        System.out.println("I will use Camel Case when writing class names.");
-        System.out.println("I will use lower Camel Case when writing function names.");
-        System.out.println("I will use lower Camel Case when writing variables names.\n");
+      printLines();
+      printLines();
+      printLines();
+      printLines();
+      printLines();
+      printLines();
 
-        // Task 2 – call the function 6 times. 
+
+        //what Bart did so far
+        // System.out.println("I will not copy and paste code.");
+        // System.out.println("I will use Camel Case when writing class names.");
+        // System.out.println("I will use lower Camel Case when writing function names.");
+        // System.out.println("I will use lower Camel Case when writing variables names.\n");
+
    }
 
-
-
-    //Task 1 - Make a function here. See the doc comment for details. 
-
-    /**    
-      * Funtion name: printLines
-      *
-      * Inside the function:
-      *   1. prints the four lines       
-      */
-
+   public static void printLines(){
+    System.out.println("I will not copy and paste code.");
+    System.out.println("I will use Camel Case when writing class names.");
+    System.out.println("I will use lower Camel Case when writing function names.");
+    System.out.println("I will use lower Camel Case when writing variables names.\n");
+   }
 
 }
